68310f240c28dc351cdea9a3ada2e79138ace1ae
johnyob <johnyob132@gmail.com>
2018-08-08 14:26:54 +0100

    Update Verify.py. Added documentation.

--- bpl_client/commands/message/Verify.py
+++ bpl_client/commands/message/Verify.py
@@ -6,11 +6,27 @@ from bpl_client.commands.Command import Command
 class Verify(Command):
 
     def __init__(self, arguments):
+        """
+        Message verify command constructor
+        Initializes _message and _public_key.
+
+        :param arguments: Contains list of arguments parsed from docopt (list)
+        """
+
         super().__init__(arguments)
         self._message = sha256(self._arguments["<message>"].encode())
         self._public_key = self._arguments["<publicKey>"]
 
     def run(self):
+        """
+        Run method for message verify command.
+        Prints prompt for the user to enter message signature.
+        Uses bpl_lib.crypto.Signature to verify message.
+        Then displays the result "Signature is valid" or "Signature is invalid" based on result from Signature.verify.
+
+        :return: (None)
+        """
+
         signature = input("Enter signature: ")
 
         valid = Signature.verify(self._public_key, self._message, signature)
