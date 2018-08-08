5abf6079ebad752e9c0bb66b3299435fd2b3069a
johnyob <johnyob132@gmail.com>
2018-08-08 10:38:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+requires = [
+    "docopt",
+    "bpl-lib>=0.0.14",
+    "bpl-api",
+    "ascii-table"
+]
+
+packages = [
+    "bpl_client",
+    "bpl_client.network",
+    "bpl_client.helpers",
+    "bpl_client.commands",
+    "bpl_client.commands.account",
+    "bpl_client.commands.message",
+    "bpl_client.commands.network",
+    "bpl_client.commands.network.config"
+]
+
+setuptools.setup(
+    name="bpl-client",
+    version="0.0.6",
+    author="Alistair O'Brien",
+    author_email="alistair.o'brien@ellesmere.com",
+    description="A CLI Client for the Blockpool Blockchain.",
+    long_description=long_description,
+    include_package_data=True,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DuneRoot/bpl-cli",
+    packages=packages,
+    install_requires=requires,
+    entry_points={
+        "console_scripts": [
+            "bpl-cli=bpl_client.__main__:main"
+        ]
+    }
+)
