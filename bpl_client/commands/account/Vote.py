d5728ed20d5cc5d6af7ef7b07305ed3a0f3bb0a9
johnyob <johnyob132@gmail.com>
2018-08-08 14:26:39 +0100

    Update Vote.py. Added documentation.

--- bpl_client/commands/account/Vote.py
+++ bpl_client/commands/account/Vote.py
@@ -1,21 +1,42 @@
 from getpass import getpass
 
-from bpl_api import Client
-from bpl_lib.network import Network as NetworkInterface
 from bpl_lib.transactions import Vote as VoteTransaction
+from bpl_lib.network import Network as NetworkInterface
+from bpl_api import Client
 
-from bpl_client.commands.Command import Command
 from bpl_client.helpers.Exceptions import BPLClientAccountsException
 from bpl_client.network.NetworkConfig import NetworkConfig
+from bpl_client.commands.Command import Command
 
 
 class Vote(Command):
 
     def __init__(self, arguments):
+        """
+        Account vote command constructor
+        Initializes _username
+
+        :param arguments: contains list of arguments parsed from docopt (list)
+        """
+
         super().__init__(arguments)
         self._username = self._arguments["<username>"]
 
     def run(self):
+        """
+        Run method for account vote command.
+        Verifies that self._username is the name of a valid delegate.
+        If self._username is invalid then a BPLClientAccountsException is raised.
+        Otherwise the user is prompted for the secret passphrase (input is masked).
+        Then the user must confirm the transaction. If the user aborts the transaction then "Aborted." is printed and
+        the transaction isn't sent.
+        Otherwise the transaction is constructed using bpl_lib.transactions.Vote and sent using bpl_api.
+        If the transaction is successful then "Transaction sent successfully with id x" is displayed, otherwise
+        "Failed to sent transaction: x" is displayed.
+
+        :return: (None)
+        """
+
         bpl_transactions = Client(NetworkConfig.get_peer()).api("transactions")
         NetworkInterface.use(NetworkConfig.get_peer())
 
@@ -26,7 +47,6 @@ class Vote(Command):
             })
 
         votes = ["+" + self._get_public_key()]
-        #secret_passphrase = input("Enter secret passphrase: ")
         secret_passphrase = getpass(prompt="Enter secret passphrase: ")
 
         if input("Confirm (y/n): ").lower() not in ["y", "yes"]:
@@ -42,7 +62,19 @@ class Vote(Command):
             print("Failed to send transaction: {0}.".format(response["error"]))
 
     def _verify_delegate(self):
+        """
+        Used to verify whether self._username stores the name of a valid delegate
+
+        :return: (boolean)
+        """
+
         return Client(NetworkConfig.get_peer()).api("delegates").delegate(self._username)["success"]
 
     def _get_public_key(self):
+        """
+        Used to return the public key of the delegate
+
+        :return: (string)
+        """
+
         return Client(NetworkConfig.get_peer()).api("delegates").delegate(self._username)["delegate"]["publicKey"]
