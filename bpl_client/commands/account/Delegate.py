6734b80446571522f8e2f506b22c40386f37e3af
johnyob <johnyob132@gmail.com>
2018-08-08 14:25:54 +0100

    Update Delegate.py. Added documentation.

--- bpl_client/commands/account/Delegate.py
+++ bpl_client/commands/account/Delegate.py
@@ -1,24 +1,42 @@
 from getpass import getpass
 
-from bpl_api import Client
-from bpl_lib.network import Network as NetworkInterface
 from bpl_lib.transactions import Delegate as DelegateTransaction
+from bpl_lib.network import Network as NetworkInterface
+from bpl_api import Client
 
-from bpl_client.commands.Command import Command
 from bpl_client.network.NetworkConfig import NetworkConfig
+from bpl_client.commands.Command import Command
 
 
 class Delegate(Command):
 
     def __init__(self, arguments):
+        """
+        Account delegate command constructor
+        Initializes _username
+
+        :param arguments: Contains list of arguments parsed from docopt (list)
+        """
+
         super().__init__(arguments)
         self._username = self._arguments["<username>"]
 
     def run(self):
+        """
+        Run method for account delegate command.
+        The user is prompted for the secret passphrase (input is masked).
+        Then the user must confirm the transaction. If the user aborts the transaction then "Aborted." is printed and
+        the transaction isn't sent.
+        Otherwise the transaction is constructed using bpl_lib.transactions.Delegate and sent using bpl_api.
+        If the transaction is successful then "Transaction sent successfully with id x" is displayed, otherwise
+        "Failed to sent transaction: x" is displayed.
+
+        :return: (None)
+        """
+
         bpl_transactions = Client(NetworkConfig.get_peer()).api("transactions")
         NetworkInterface.use(NetworkConfig.get_peer())
 
-        #secret_passphrase = input("Enter secret passphrase: ")
         secret_passphrase = getpass(prompt="Enter secret passphrase: ")
 
         if input("confirm (y/n): ").lower() not in ["y", "yes"]:
