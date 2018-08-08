f61ce3fb8bb9a3eb6252b0aa9c61edfe956a4acf
johnyob <johnyob132@gmail.com>
2018-08-08 14:26:17 +0100

    Update bpl_client.commands.account.Status.py. Added documentation.

--- bpl_client/commands/account/Status.py
+++ bpl_client/commands/account/Status.py
@@ -1,24 +1,44 @@
 from ascii_table import Table
 from bpl_api import Client
 
-from bpl_client.commands.Command import Command
 from bpl_client.helpers.Exceptions import BPLClientAccountsException
-from bpl_client.helpers.Util import SortedDictionary
 from bpl_client.network.NetworkConfig import NetworkConfig
+from bpl_client.helpers.Util import SortedDictionary
+from bpl_client.commands.Command import Command
 
 
 class Status(Command):
 
     def __init__(self, arguments):
+        """
+        Account status command constructor
+        Initializes _bpl_accounts and _address
+
+        :param arguments: Contains list of arguments parsed from docopt (list)
+        """
+
         super().__init__(arguments)
         self._bpl_accounts = Client(NetworkConfig.get_peer()).api("accounts")
         self._address = self._arguments["<address>"]
 
     def run(self):
+        """
+        Run method for account status command
+        Displays the status of the specified account and any delegates the account voted for.
+
+        :return: (None)
+        """
+
         self._get_account()
         self._get_delegate()
 
     def _get_account(self):
+        """
+        Used to fetch the status of the account and then display it using a ascii_table.Table object
+
+        :return: (None)
+        """
+
         account = self._bpl_accounts.account(self._address)
 
         if not account["success"]:
@@ -34,9 +54,16 @@ class Status(Command):
         del account["address"]
 
         print("\nAddress Summary")
-        print(Table([account.headers(), account.values()]))
+        print(Table([account.names(), account.values()]))
 
     def _get_delegate(self):
+        """
+        Used to fetch the status of any delegates the account voted for and display them individually using a
+        ascii_table.Table object
+
+        :return: (None)
+        """
+
         votes = self._bpl_accounts.votes(self._address)
 
         if not votes["success"]:
@@ -50,5 +77,5 @@ class Status(Command):
             del delegate["address"]
             del delegate["publicKey"]
 
-            print("\nDelegate ({0})".format(delegate["username"]))
-            print(Table([delegate.headers(), delegate.values()]))
+            print("\nVoted Delegate ({0})".format(delegate["username"]))
+            print(Table([delegate.names(), delegate.values()]))
