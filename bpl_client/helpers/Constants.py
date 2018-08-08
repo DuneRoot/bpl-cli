ee6d54dfaeee6926e0128cc7eafca5e752cc49de
johnyob <johnyob132@gmail.com>
2018-08-08 10:33:43 +0100

    Create Constants.py

---
+++ bpl_client/helpers/Constants.py
@@ -0,0 +1,12 @@
+import os
+
+from bpl_client import ROOT
+
+COMMANDS_JSON = os.path.join(ROOT, "commands.json")
+NETWORK_CONFIG = os.path.join(ROOT, "network\\config.json")
+
+PEER_ADDRESS_REGEX = r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})$"
+VERSION_REGEX = r"^(\d{1,3})$"
+EPOCH_REGEX = r"^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})$"
+
+BPL = 100000000
