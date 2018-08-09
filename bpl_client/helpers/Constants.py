bc31a4f9379e321b15d27368a3c1fce9affdf36b
johnyob <johnyob132@gmail.com>
2018-08-09 14:09:29 +0100

    Update Constants.py. Added NETWORK_CONFIGS file path. Network config interface update (0.0.8).

--- bpl_client/helpers/Constants.py
+++ bpl_client/helpers/Constants.py
@@ -4,6 +4,7 @@ from bpl_client import ROOT
 
 COMMANDS_JSON = os.path.join(ROOT, "commands.json")
 NETWORK_CONFIG = os.path.join(ROOT, "network\\config.json")
+NETWORK_CONFIGS = os.path.join(ROOT, "network\\configs.json")
 
 PEER_ADDRESS_REGEX = r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})$"
 VERSION_REGEX = r"^(\d{1,3})$"
